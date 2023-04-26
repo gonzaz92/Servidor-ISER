e9f1888cbe71e061d8e378f0ba6bd9c6d3eb99fe
Gonza.z92 <g.olalla92@gmail.com>
2023-04-26 07:15:47 -0300

    Primera subida

---
+++ Base_datos/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from Base_datos.models import Locutor_nacional, Locutor_local, Operador_nacional_radio, Operador_nacional_tv, Operador_nacional_planta, Operador_local_radio, Operador_local_tv, Operador_local_planta
+from Base_datos.forms import UsuarioForm
+from django.db.models import Q
+
+def index(request):
+    return render(request, 'Base_datos/index.html')
+
+def locutores(request):
+    return render(request, 'Base_datos/locutores_nacionales.html')
+
+class UserSingUp(LoginRequiredMixin, CreateView):
+    form_class = UsuarioForm
+    template_name = 'registration/singup.html'
+    success_url = reverse_lazy('mi_blog_listar')
+
+class UserLogin(LoginView):
+    next_page = reverse_lazy('mi_blog_listar')
+
+class UserLogout(LogoutView):
+    next_page = reverse_lazy('mi_blog_index')
+
+
+class CrearLocutorNacional(CreateView):
+    model = Locutor_nacional
+    success_url = reverse_lazy('locutores_nacionales')
+    fields = '__all__'
+
+class ListarLocutorNacional(ListView):
+    model = Locutor_nacional
+    fields = Locutor_nacional.apellido, Locutor_nacional.nombre, Locutor_nacional.DNI, Locutor_nacional.habilitación, Locutor_nacional.instituto, Locutor_nacional.expediente, Locutor_nacional.disposición
+
+class VerLocutorNacional(DetailView):
+    model = Locutor_nacional
