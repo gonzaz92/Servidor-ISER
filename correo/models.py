db501a8dbad5b4df7762d48ba225f24191c6228f
gonzaz92 <gonza.zorro92@gmail.com>
2023-10-11 17:52:19 -0300

    Actualización correo

--- correo/models.py
+++ correo/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from expedientes.models import categorias, validar_numero
 
 class Correo(models.Model):
     envio = models.DateField(verbose_name='Fecha de Envío')
     destinatario = models.CharField(max_length=100, verbose_name='Apellido')
     nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    dni = models.IntegerField(validators=[MaxValueValidator(999999999)], verbose_name='D.N.I.')
     direccion = models.CharField(max_length=100, verbose_name='Dirección de Envío')
+    localidad = models.CharField(max_length=100, null='True', blank=True, verbose_name='Localidad')
+    provincia = models.CharField(max_length=100, null='True', blank=True, verbose_name='Provincia')
     seguimiento = models.CharField(max_length=15, verbose_name='Número de Seguimiento')
+    categoria = models.CharField(max_length=43, choices=categorias, verbose_name='Categoría')
+    telefono = models.CharField(max_length=10, null='True', blank=True, verbose_name='Teléfono')
+    mail = models.CharField(max_length=50, null='True', blank=True, verbose_name='e-mail')
+    observaciones = models.TextField(max_length=200, verbose_name='Observaciones')
     acuse = models.CharField(max_length=33, null='True', blank=True, verbose_name='Acuse de Recibo')
