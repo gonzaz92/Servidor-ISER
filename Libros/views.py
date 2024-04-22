a949bd32f8eaf20b59c843c658fc8f35c49c8b87
Gonza.z92 <gonza.zorro92@gmail.com>
2024-04-22 01:41:08 -0300

    Se agregaron permisos

--- Libros/views.py
+++ Libros/views.py
@@ -8,19 +8,41 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import JsonResponse
 
+########### Permisos ##########
+
+def nuevo_libro(user):
+    return user.has_perm('Libros.add_libro')
+
+def ver_libro(user):
+    return user.has_perm('Libros.view_libro')
+
+
+def nueva_acta(user):
+    return user.has_perm('Libros.add_acta')
+
+def actualizar_acta(user):
+    return user.has_perm('Libros.change_acta')
+
+#################################################################
+
+@login_required
 def libros(request):
     return render(request, 'Libros/libros.html')
 
+@method_decorator(login_required, name='get')
+@method_decorator(user_passes_test(nuevo_libro), name='get')
 class NuevoLibro(CreateView):
     model = Libro
     fields = '__all__'
     success_url = reverse_lazy('libros')
 
+@method_decorator(login_required, name='get')
 class ListarLibros(ListView):
     model = Libro
     fields = '__all__'
     success_url = reverse_lazy('libros')
 
+@method_decorator(login_required, name='get')
 class ActualizarLibro(UpdateView):
     model = Libro
     fields = '__all__'
@@ -28,6 +50,8 @@ class ActualizarLibro(UpdateView):
 
 #################Actas#####################
 
+@method_decorator(login_required, name='get')
+@method_decorator(user_passes_test(nueva_acta), name='get')
 class NuevaActa(CreateView):
     template_name = 'Libros/acta_form.html'
     form_class = ActaForm
@@ -53,6 +77,7 @@ class NuevaActa(CreateView):
         context['firmas'] = self.firmo()
         return context
 
+@method_decorator(login_required, name='get')
 class ListarActas(ListView):
     model = Acta
     fields = '__all__'
@@ -62,9 +87,12 @@ class ListarActas(ListView):
         nombre = self.kwargs['nombre']
         return Acta.objects.filter(instituto = nombre).order_by('-fecha')
 
+@method_decorator(login_required, name='get')
 class DetalleActa(DetailView):
     model = Acta
 
+@method_decorator(login_required, name='get')
+@method_decorator(user_passes_test(actualizar_acta), name='get')
 class ActualizarActa(UpdateView):
     model = Acta
     success_url = reverse_lazy('libros')
